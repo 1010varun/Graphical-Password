540d1a279f13a979d221e1b20ff3eaa84e221d4d
1010varun <varunagarwal10.2@gmail.com>
2023-04-07 23:58:31 +0530

    api to fetch image

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+// COMPONENTS
+import Navbar from "./components/Navbar";
+
+
+const App = () => {
+
+  const [email, setEmail] = useState("");
+  const [theme, setTheme] = useState("");
+
+  const handelSubmit = async () => {
+    const url = "http://localhost:5000/signup";
+    const data = { email, theme };
+    axios.post(url, data);
+    setTheme("");
+    setEmail("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Navbar />
+
+      <div className="mt-48 flex flex-row justify-center">
+        <div class="mr-5 relative z-0 w-3/12 mb-6 group">
+          <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_email"
+            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </div>
+        <div class="ml-5 relative z-0 w-3/12 mb-6 group">
+          <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_email"
+            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Theme
+          </label>
+        </div>
+      </div>
+
+      <div className="text-center">
+        <button
+          type="button"
+          onClick={handelSubmit}
+          class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 mt-10"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Select Image
+        </button>
+      </div>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,25 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-import { createApi } from 'unsplash-js';
-require('dotenv').config();
+const { urlencoded } = require("express");
+const cors = require("cors");
+const fetch  = require("node-fetch");
+const {createApi}  = require("unsplash-js");
+require("dotenv").config();
 
-app.get('/', function(req, res) {
-    res.send("welcome to node server");
-})
+global.fetch = fetch;
 
-// const unsplash = createApi({
-//     accessKey: process.env.ACCESS_KEY,
-//   });
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
 
-app.post('/signup', (req, res) => {
-    const {email} = req.body;
-    console.log(email);
-})
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.get("/", function (req, res) {
+  res.send("welcome to node server");
+});
+
+const unsplash = createApi({ accessKey: process.env.ACCESS_KEY });
+
+
+app.post("/signup", (req, res) => {
+  const { email, theme } = req.body;
+  console.log(email, theme);
+    if ({ email }) {
+        photo(theme);
+        res.send("email recieved");
+  } else {
+        res.send("error");
+  }
+});
+
+
+const photo = async (theme) => {
+  const photos = await unsplash.search.getPhotos({ query: theme });
+  console.log(photos.response.results.length);
+};
+
 
 app.listen(process.env.PORT, (err) => {
-    if (process.env.PORT){
-        console.log("app is listening to port " + process.env.PORT);
-    }   else {
-        console.log(err);
-    }
-})
+  if (process.env.PORT) {
+    console.log("app is listening to port " + process.env.PORT);
+  } else {
+    console.log(err);
+  }
+});
